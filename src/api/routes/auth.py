c0b66417194c68d784b3da535516fb64cecfaa60
Pham-Ngoc-Huy <huypham10122001@gmail.com>
2026-08-29 10:29:33 +0700

    feat(api): restore authentication routes and schemas

---
+++ src/api/routes/auth.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+from src.api.schemas.auth import LoginRequest, LoginResponse
+from src.api.services.auth import authenticate_user
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"],
+)
+
+
+@router.post("/login")
+def login(requests: LoginRequest):
+    user = authenticate_user(
+        username=requests.username,
+        password=requests.password,
+    )
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password",
+        )
+
+    return user
+
+@router.post("/logout")
+def logout(username:str, logout: bool):
+    return {
+        "username": username,
+        "message": "logout successful"
+    }
---
+++ src/api/services/auth.py
@@ -0,0 +1,8 @@
+def authenticate_user(username:str, password:str):
+    if username == "admin123" and password == "admin123":
+        return{
+            "id": 1,
+            "username": "admin123",
+            "collection": "admin123",
+        }
+    return None
